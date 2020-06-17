2c2c9d7c874a285c4c46f4517b44f274f9b2b2be
LeKinchi <32843930+LeKinchi@users.noreply.github.com>
2020-06-16 22:45:50 -0400

    Added route for Recommendations page (#20)
    * Added route for recommendations page
    
    * Created Recommendations view

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Recommendations from "../views/Recommendations.vue";
+
 
 Vue.use(VueRouter);
 
@@ -18,6 +20,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+ {
+    path: "/recommendations",
+    name: "Recommendations",
+    component: Recommendations
   }
 ];
 
