641dfb9e2817e2527404ac3fbb98e2ef312fb60b
LeKinchi <32843930+LeKinchi@users.noreply.github.com>
2020-06-16 22:45:50 -0400

    Added route for Recommendations page (#20)
    * Added route for recommendations page
    
    * Created Recommendations view

---
+++ app/src/views/Recommendations.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="recommendations">
+    <h1>This page will be built later</h1>
+  </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Recommendations",
+    };
+
+</script>
