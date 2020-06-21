0687f161e12cdd1499c3308a1cd4596aa0a2424b
John Poon <hpoon16@huntersoe.org>
2020-06-20 21:29:55 -0400

    Added navbar, content, footer components (#47)

---
+++ app/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Binger ©2020 Created by idk
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style></style>
---
+++ app/src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-layout-header class="header" :style="{ padding: '0px 15px 0px 15px' }">
+    <router-link class="logo" to="/"></router-link>
+    <h1 class="app-name">Binger</h1>
+    <a-menu theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+      <a-menu-item key="1">
+        <router-link to="/login">Login</router-link>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <router-link to="/signup">Register</router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  /* position: fixed; */
+  /* z-index: 1; */
+  width: 100%;
+}
+
+.logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+}
+
+.app-name {
+  color: white;
+  margin: 0px 25px 0px 25px;
+}
+
+router-link-exact-active {
+  color: #42b983;
+}
+</style>
