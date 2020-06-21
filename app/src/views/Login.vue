c46114972b40c7b4c0e6581486f976737fccb364
John Poon <hpoon16@huntersoe.org>
2020-06-20 23:11:50 -0400

    Added Login component (#48)

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,22 +1,20 @@
 <template>
   <a-layout :style="{ minHeight: '100%', overflow: 'auto' }">
     <NavBar />
-    <a-layout-content>
-      <Content msg="This is log in page" />
-    </a-layout-content>
+    <LoginForm />
   </a-layout>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavBar from "@/components/NavBar.vue";
-import Content from "@/components/Content.vue";
+import LoginForm from "@/components/LoginForm.vue";
 
 export default {
   name: "Login",
   components: {
     NavBar,
-    Content
+    LoginForm
   }
 };
 </script>
