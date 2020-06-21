3dda93909ec4f273967fcfa0e20c4196774b37c8
ShinValor <hpoon16@huntersoe.org>
2020-06-21 01:39:07 -0400

    Added card component

---
+++ app/src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+  <a-card class="profile" hoverable>
+    <img
+      slot="cover"
+      alt="example"
+      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+    />
+    <a-card-meta :title="name" :description="desc">
+      <a-avatar
+        slot="avatar"
+        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+      />
+    </a-card-meta>
+  </a-card>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: String,
+    desc: String,
+    photoUrl: String,
+    avatarUrl: String
+  }
+};
+</script>
+
+<style>
+.profile {
+  height: 450px;
+  width: 500px;
+  margin: 50px !important;
+}
+
+@media screen and (max-width: 500px) {
+  /* applies styles to any device screen sizes below 800px wide */
+
+  .profile {
+    height: 400px;
+    width: 300px;
+    margin: 15px auto !important;
+  }
+}
+
+@media screen and (max-width: 300px) {
+  /* applies styles to any device screen sizes below 800px wide */
+
+  .profile {
+    height: 300px;
+    margin: 5px auto !important;
+  }
+}
+</style>
