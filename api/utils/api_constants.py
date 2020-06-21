6155d9e609c62327b197e82017578ada013bca69
tweekes1 <42299090+tweekes1@users.noreply.github.com>
2020-06-21 11:19:00 -0400

    Create API Handler responsible for making API calls (#41)
    * I am still learning this
    
    * Added the grequest module and updated the requirements file
    
    * Removed test sentence from README again
    
    * Created utils folder within the flask app. Three python files are necessary for manipulating API responses in a clean way.

---
+++ api/utils/api_constants.py
@@ -0,0 +1,85 @@
+"""
+Module that will hold the constants for the application.
+
+os module is used because the api key requires is stored in an environment 
+variable.
+"""
+
+import os
+
+"""
+All the available genres from TMDB, Placed in constant var for easy access
+instead of requesting API everytime a genre id needs to be resolved.
+Acquired using TMDB API endpoints:
+
+/genre/movie/list
+/genre/tv/list
+"""
+GENRE_IDS_TO_NAME = {
+    12    : "Adventure",
+    14    : "Fantasy",
+    16    : "Animation",
+    18    : "Drama",
+    27    : "Horror",
+    28    : "Action",
+    35    : "Comedy",
+    36    : "History",
+    37    : "Western",
+    53    : "Thriller",    
+    80    : "Crime",
+    99    : "Documentary",
+    878   : "Science Fiction",
+    9648  : "Mystery",
+    10402 : "Music",
+    10749 : "Romance",
+    10751 : "Family",
+    10752 : "War",
+    10759 : "Action & Adventure",
+    10762 : "Kids", 
+    10763 : "News",
+    10764 : "Reality",
+    10765 : "Sci-Fi & Fantasy",
+    10766 : "Soaps", 
+    10767 : "Talk",
+    10768 : "War & Politics",
+    10770 : "TV Movie"
+}
+
+"""
+A dictionary of GENRE_IDS but has its keys and values swapped for resolving a
+genre's name to an ID.
+"""
+GENRE_NAMES_TO_IDS = dict((value, key) for key, value in GENRE_IDS_TO_NAME.items())
+
+"""
+API Key needed for accessing the TMDB API. Assumes that the key is saved as an environment var
+under the name 'TMDB_KEY'.
+"""
+API_KEY = os.environ.get("TMDB_KEY")
+
+"""
+The default payload options used for http requests. Other options will be appended as needed
+via deep copying.
+"""
+DEFAULT_OPTIONS = {
+    "api_key"     : API_KEY,
+    "language"    : "en-US",
+    "sort_by"     : "popularity.desc"
+}
+
+""" 
+Base URL used by TMDB to resolve "show's" poster.
+"""
+TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/w500{}"
+
+""" 
+Base URLs used by TMDB to discover initial "shows".
+"""
+MOVIE_DISCOVER_URL = "https://api.themoviedb.org/3/discover/movie"
+TV_SHOW_DISCOVER_URL = "https://api.themoviedb.org/3/discover/tv"
+
+"""
+Base URLs used by TMDB to get recommendations based on "show" ids.
+"""
+MOVIE_RECOMMENDATION_URL = "https://api.themoviedb.org/3/movie/{}/recommendations"
+TV_SHOW_RECOMMENDATION_URL = "https://api.themoviedb.org/3/tv/{}/recommendations"
---
+++ api/utils/api_handler.py
@@ -0,0 +1,181 @@
+"""
+Module will handle fetching data from the TMDB API.
+
+Module will mainly be responsible for sending requests and receiving 
+data from API. API requests will be sent asynchronously using the 
+grequests module with the help of modules from the requests module.
+
+Docummentation: https://developers.themoviedb.org/3/getting-started/introduction
+"""
+
+import copy
+import grequests
+import json
+import requests
+import shows
+
+import api_constants as consts
+
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
+class APIHandler: 
+    """
+    Wrapper class to make API calls easy.
+
+    Attributes:
+        number_of_sessions: The number of sessions that will be used to asynchronously fetch data 
+        from the API.
+        sessions: A list of requests.Sessions that are responsible for fetching data.
+        retries: Retry object that defines how http retries will be handled.
+    """
+
+    def __init__(self, number_of_sessions=10):
+        """
+        Initializes the api handler. 
+
+        By default, number_of_sessions is 10 but you can pass an optional int to alter
+        the number of sessions the APIHandler will use. Each of the sessions defined 
+        will also be sent over https with a maximum number of retries.
+        """
+
+        self.number_of_sessions = number_of_sessions
+        self.sessions = [requests.Session() for i in range(self.number_of_sessions)]
+        self.retries = Retry(total=3, 
+                             backoff_factor=0.1,
+                             status_forcelist=[500, 502, 503, 504])   
+
+        for session in self.sessions:
+            session.mount("https://", HTTPAdapter(max_retries=self.retries))
+    
+    def async_request(self, url, session_id, params):
+        """
+        Fuction that returns a async request using the 'grequests' module.
+
+        Args: 
+            url: The url to the endpoint of the API
+            sessions_id: An int that will decide which of the sessions will be used.
+            session_id is modulo by the attribute 'number_of_sessions
+        """
+
+        return grequests.get(url, 
+                             session=self.sessions[session_id % self.number_of_sessions],
+                             params=params)
+    
+    def build_show_list(self, responses, is_movie):
+        """
+        Function that will take API responses and builds a list of "shows"
+
+        Args: 
+            responses: API responses that are received from TMDB requests.
+            is_movie: Boolean to determine the type of "show" list is being created.
+
+        Returns: 
+            list_of_shows: A list of "show" objects that are to be returned.
+        """
+
+        list_of_shows = []
+        
+        for response in responses:
+            api_results = json.loads(response.text)["results"]
+            list_of_shows.extend(shows.create_show_list(api_results, is_movie))
+
+        return list_of_shows
+    
+    def get_initial_show_requests(self, genres, number_of_pages, is_movie):
+        """
+        Function is responsible for building a list of requests that will be sent 
+        to the TMDB API.
+
+        Args:
+            genres: List of genre ids
+            number_of_pages: Number of pages that will be requested from the API. The 
+            maximum number of results returned per page is 20.
+            is_movie: Boolean to decide which endpoint to make request for. Movie or TV.
+
+        Returns:
+            show_requests: A list of requests to be sent to the API.
+        """
+        session_id = 0
+        show_requests = []
+
+        options = copy.deepcopy(consts.DEFAULT_OPTIONS)
+        
+        for i in range(1, (number_of_pages + 1)):
+            for genre in genres:
+                options["with_genres"] = genre
+                options["page"] = i
+
+                if is_movie: 
+                    options["release_date.lte"] = 2020
+                    api_url = consts.MOVIE_DISCOVER_URL
+                else: 
+                    options["first_air_date.lte"] = 2020
+                    api_url = consts.TV_SHOW_DISCOVER_URL
+
+                show_requests.append(self.async_request(api_url, session_id, options))
+                session_id += 1
+        
+        return show_requests
+     
+    def get_initial_shows_by_genre(self, genres, number_of_pages):
+        """
+        Handles the initial retrieval of Movies and TV Shows from the TMDB API and will
+        return a list of "show" objects. Utilizes the class function 'get_initial_show_requests'
+        to return a list of the requests that will be set to the API.
+
+        Args:
+            genres: The id(s) that are to be queried.
+            number_of_pages: How many pages each are requested for each genre. Maximum returned per page is 20. 
+
+        Returns: 
+            show_list: Returns a list of 'show' objects
+        """
+        
+        movie_requests = self.get_initial_show_requests(genres, number_of_pages, True)
+        tv_show_requests = self.get_initial_show_requests(genres, number_of_pages, False)
+
+        movie_responses = grequests.map(movie_requests, size=self.number_of_sessions)
+        tv_show_responses = grequests.map(tv_show_requests, size=self.number_of_sessions)
+
+        show_list = []
+        show_list.extend(self.build_show_list(movie_responses, True))
+        show_list.extend(self.build_show_list(tv_show_responses, False))
+        
+        return show_list
+
+    def get_recommendation_by_show(self, show, number_of_pages=1):
+        """
+        Function that will request "show" recommendations from the TMDB API.
+
+        Args: 
+            show: A show object
+            number_of_pages: The number of pages that will be requested.
+
+        Returns: 
+            show_list: A list of show objects that are recommendation from the 
+            arg 'show'.
+        """
+
+        session_id = 0
+        recommendation_requests = []
+        
+        options = copy.deepcopy(consts.DEFAULT_OPTIONS)
+        
+        if show.is_movie:
+            api_url = consts.MOVIE_RECOMMENDATION_URL
+        else:
+            api_url = consts.TV_SHOW_RECOMMENDATION_URL
+
+        for i in range(1, (number_of_pages + 1)):
+            options["page"] = i
+            recommendation_requests.append(self.async_request(api_url.format(show.id), 
+                                            session_id, options))
+            session_id += 1
+
+        recommendation_responses = grequests.map(recommendation_requests, size=self.number_of_sessions)
+
+        show_list = []    
+        show_list.extend(self.build_show_list(recommendation_responses, show.is_movie))
+
+        return show_list            
