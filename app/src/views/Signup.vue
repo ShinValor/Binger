bd424496d01accc1189d1ec0941b049c377b429b
ShinValor <hpoon16@huntersoe.org>
2020-06-20 23:18:12 -0400

    Revert "Added Signup component"
    Accidently pushed to dev
    
    This reverts commit 43ee68d617fb7261a7a3f262e13e32a5be1f1de5.

--- app/src/views/Signup.vue
+++ app/src/views/Signup.vue
@@ -1,20 +1,22 @@
 <template>
   <a-layout :style="{ minHeight: '100%', overflow: 'auto' }">
     <NavBar />
-    <SignupForm />
+    <a-layout-content>
+      <Content msg="This is sign up page" />
+    </a-layout-content>
   </a-layout>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavBar from "@/components/NavBar.vue";
-import SignupForm from "@/components/SignupForm.vue";
+import Content from "@/components/Content.vue";
 
 export default {
   name: "Signup",
   components: {
     NavBar,
-    SignupForm
+    Content
   }
 };
 </script>
