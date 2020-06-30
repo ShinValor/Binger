24ec7b222c46e6afef75f7d80c6fa69829b123e4
ShinValor <hpoon16@huntersoe.org>
2020-06-30 10:55:48 -0400

    update

--- test/api/app.py
+++ test/api/app.py
@@ -1,14 +1,12 @@
 import os
 from flask import Flask, request, jsonify
-from firebase_admin import credentials, firestore, initialize_app
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
-
-# #Initialize Firestore DB
-cred = credentials.Certificate('./key.json')
-default_app = initialize_app(cred)
-db = firestore.client()
-# todo_ref = db.collection('todos')
 
 @app.route('/')
 def index():
