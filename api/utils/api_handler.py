573b9ad172217c14b420d8310fad9d7e776af3d4
Tarik <tarik.weekes73@myhunter.cuny.edu>
2020-07-03 16:11:44 -0400

    Fixed bug in api handler that was resulting in duplicate items from the api response.

--- api/utils/api_handler.py
+++ api/utils/api_handler.py
@@ -5,7 +5,7 @@ Module will mainly be responsible for sending requests and receiving
 data from API. API requests will be sent asynchronously using the 
 grequests module with the help of modules from the requests module.
 
-Docummentation: https://developers.themoviedb.org/3/getting-started/introduction
+Reference: https://developers.themoviedb.org/3/getting-started/introduction
 """
 
 import copy
@@ -55,7 +55,7 @@ class APIHandler:
         Args: 
             url: The url to the endpoint of the API
             sessions_id: An int that will decide which of the sessions will be used.
-            session_id is modulo by the attribute 'number_of_sessions
+            session_id is modulo by the attribute 'number_of_sessions'
         """
 
         return grequests.get(url, 
@@ -64,14 +64,14 @@ class APIHandler:
     
     def build_show_list(self, responses, is_movie):
         """
-        Function that will take API responses and builds a list of "shows"
+        Function that will take API responses and builds a list of "Shows"
 
         Args: 
             responses: API responses that are received from TMDB requests.
-            is_movie: Boolean to determine the type of "show" list is being created.
+            is_movie: Boolean to determine the type of "Show" list is being created.
 
         Returns: 
-            list_of_shows: A list of "show" objects that are to be returned.
+            list_of_shows: A list of "Show" objects that are to be returned.
         """
 
         list_of_shows = []
@@ -91,37 +91,39 @@ class APIHandler:
             genres: List of genre ids
             number_of_pages: Number of pages that will be requested from the API. The 
             maximum number of results returned per page is 20.
-            is_movie: Boolean to decide which endpoint to make request for. Movie or TV.
+            is_movie: Boolean to decide which endpoint to make request for. Movie or TV Show.
 
         Returns:
             show_requests: A list of requests to be sent to the API.
         """
+
         session_id = 0
         show_requests = []
 
-        options = copy.deepcopy(consts.DEFAULT_OPTIONS)
-        
         for i in range(1, (number_of_pages + 1)):
             for genre in genres:
+                options = copy.deepcopy(consts.DEFAULT_OPTIONS)
                 options["with_genres"] = genre
                 options["page"] = i
 
                 if is_movie: 
-                    options["release_date.lte"] = 2020
+                    options["release_date.lte"] = "2020-06-01"
+                    options["release_date.gte"] = "1985-01-01"
                     api_url = consts.MOVIE_DISCOVER_URL
                 else: 
-                    options["first_air_date.lte"] = 2020
+                    options["first_air_date.lte"] = "2020-06-01"
+                    options["first_air_date.gte"] = "1985-01-01"
                     api_url = consts.TV_SHOW_DISCOVER_URL
 
                 show_requests.append(self.async_request(api_url, session_id, options))
                 session_id += 1
-        
+
         return show_requests
      
     def get_initial_shows_by_genre(self, genres, number_of_pages):
         """
         Handles the initial retrieval of Movies and TV Shows from the TMDB API and will
-        return a list of "show" objects. Utilizes the class function 'get_initial_show_requests'
+        return a list of "Show" objects. Utilizes the class function 'get_initial_show_requests'
         to return a list of the requests that will be set to the API.
 
         Args:
@@ -129,7 +131,7 @@ class APIHandler:
             number_of_pages: How many pages each are requested for each genre. Maximum returned per page is 20. 
 
         Returns: 
-            show_list: Returns a list of 'show' objects
+            show_list: Returns a list of "Show" objects
         """
         
         movie_requests = self.get_initial_show_requests(genres, number_of_pages, True)
@@ -146,15 +148,15 @@ class APIHandler:
 
     def get_recommendation_by_show(self, show, number_of_pages=1):
         """
-        Function that will request "show" recommendations from the TMDB API.
+        Function that will request "Show" recommendations from the TMDB API.
 
         Args: 
-            show: A show object
+            show: A "Show" object
             number_of_pages: The number of pages that will be requested.
 
         Returns: 
-            show_list: A list of show objects that are recommendation from the 
-            arg 'show'.
+            show_list: A list of "Show" objects that are recommendation from the 
+            arg "show".
         """
 
         session_id = 0
