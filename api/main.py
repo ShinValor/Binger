75531b44d315d8b64a63e920be66685eb9596c67
junqili <39481617+junqili259@users.noreply.github.com>
2020-07-04 18:26:44 -0400

    Firestore (#75)
    * Update main.py
    
    * created /addUserShows
    
    * Update main.py
    
    * Update main.py
    
    * deleted /tmdb route
    
    * Update main.py
    
    * Update main.py
    
    * Update main.py

--- api/main.py
+++ api/main.py
@@ -10,9 +10,13 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 # Read env variables from config.py
 app.config.from_object('config.Config')
 
+
 # cred = credentials.Certificate('key.json')
 # firebase_app = initialize_app(cred)
 firebase_app = initialize_app()
+
+db = firestore.client()
+
 
 def check_token(f):
     """
@@ -44,6 +48,8 @@ def check_token(f):
         return f(*args,**kwargs)
     return wrap
 
+
+
 @app.route('/')
 def home():
     return Response(response='success',status=200)
@@ -60,30 +66,94 @@ def test():
     print("Decoded Token: ", decoded_token)
     return decoded_token
 
-@app.route('/tmdb')
-@check_token
-def tmdb():
-    # Access API handler for TMDB
-    # return shows user may be interested in
-    return Response(response='success',status=200)
 
-@app.route('/getUserShows')
+@app.route('/getUserShows',methods=['GET'])
 @check_token
 def getUserShows():
-    return Response(response='success',status=200)
+    """
+    Retrieves all tv shows user is interested in from their shows collection on firestore
+    """
+    shows_list = []
+    
     # get idToken
-    # decode idToken
-    # get all of the user's interested shows from firestore
-    # format the data into json
-    # return
+    token = request.args.get('token')
 
-@app.route('/addUserShows')
+    # decode idToken
+    decoded_token = auth.verify_id_token(token)
+    uid = decoded_token['uid']
+
+    # get all of the user's interested shows from firestore
+    user_shows = db.collection('users').document(uid).collection('shows').stream()
+
+    # Append all data into a list and return as json array
+    # If user or data doesn't exist, an empty json array will be returned
+    for show in user_shows:
+        shows_list.append(show.to_dict())
+ 
+    return jsonify(shows_list)
+
+
+
+@app.route('/getUserMovies',methods=['GET'])
+@check_token
+def getUserMovies():
+    """
+    Retrieves all movies user is interested in from their movie collection on firestore
+    """
+    movies_list = []
+
+    # get idToken
+    token = request.args.get('token')
+
+    # decode idToken
+    decoded_token = auth.verify_id_token(token)
+    uid = decoded_token['uid']
+
+    # get all of the user's interested shows from firestore
+    user_movies = db.collection('users').document(uid).collection('movies').stream()
+
+    # Append all data into a list and return as json array
+    # If user or data doesn't exist, an empty json array will be returned
+    for movie in user_movies:
+        movies_list.append(movie.to_dict())
+
+    return jsonify(movies_list)
+
+
+
+@app.route('/addUserShows',methods=['GET','POST'])
 @check_token
 def addUserShows():
+    """
+    Add Tv show data into user's show collection in firestore
+    """
+    data = request.get_json()
+    title = data['title']
+    
     # get idToken
+    token = request.args.get('token')
+
     # decode idToken
-    # add new show to user's firestore
-    # verify that it was successfully added to firestore
-    # return
+    decoded_token = auth.verify_id_token(token)
+    uid = decoded_token['uid']
+    
+    # create a reference to a new document in the user's show collection
+    doc_ref = db.collection('users').document(uid).collection('shows').document(title)
+
+    # Add the tv show data to the new document
+    doc_ref.set({
+         "id": data['id'],
+         "genres": data['genres'],
+         "overview": data['overview'],
+         "popularity": data['popularity'],
+         "poster_path": data['poster_path'],
+         "release_year": data['release_year'],
+         "score": data['score'],
+         "title": data['title'],
+         "is_movie": data['is_movie']
+    })
+    
     return Response(response='success',status=200)
+
+
 
