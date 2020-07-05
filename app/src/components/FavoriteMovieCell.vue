eed7fdda431add51cc6162473cedfb45c0d00e1b
LeKinchi <lesporis@gmail.com>
2020-07-05 19:22:18 -0400

    Created html for a cell

---
+++ app/src/components/FavoriteMovieCell.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <img :src="item.img" />
+    <br />
+    <div>
+      <a class="title">{{ item.title }}</a>
+      <div class="d-flex items-center area muted">
+        <p class="year">{{ item.release_year }}</p>
+        <p class="separator genres">{{ genres }}</p>
+      </div>
+      <p class="area plot">{{ item.overview }}</p>
+      <div class="d-flex items-center area">
+        <p class="rating" title="Rating" v-show="item.popularity">
+          <span class="rating--star"></span>
+          {{ item.rating }}
+        </p>
+      </div>
+    </div>
+  </li>
+</template>
+
