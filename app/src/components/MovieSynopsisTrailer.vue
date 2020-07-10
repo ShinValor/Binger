af09031c5300dfbb50e0bd18f16511f45d82d97d
LeKinchi <lesporis@gmail.com>
2020-07-10 14:33:53 -0400

    Created MovieSynopsisTrailer component

---
+++ app/src/components/MovieSynopsisTrailer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div class="trailer-body" style="background: #1e1d1d">
+      <div class="has-text-centered">
+        <div class="columns">
+          <div class="column vertical-align">
+            <iframe
+              allowfullscreen
+              frameborder="0"
+              height="376"
+              src="https://www.youtube.com/embed/QWbMckU3AOQ?autoplay=1&modestbranding=1"
+              style="width: 100%; min-width: 536px"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieSynopsisTrailer"
+};
+</script>
+
+<style scoped>
+.columns {
+  padding: 20px;
+  padding-bottom: 30px;
+  text-align: center;
+  /* background: #1e1d1d !important; */
+  /* position: relative; */
+  /* box-shadow: 0 0 50px 5px rgba(0, 0, 0, 0.25); */
+  /* background: linear-gradient(to bottom right, #7d1e24, #bb2d35); */
+}
+.vertical-align {
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+}
+.card {
+  margin: 1.5em;
+}
+</style>
--- app/src/views/MovieSynopsis.vue
+++ app/src/views/MovieSynopsis.vue
@@ -2,15 +2,18 @@
   <a-layout :style="{ minHeight: '100%', overflow: 'auto' }">
     <h1>Movie Synopsis Page</h1>
     <Movie_Synopsis />
+    <MovieSynopsisTrailer />
   </a-layout>
 </template>
 
 <script scoped>
 import Movie_Synopsis from "@/components/MovieSynopsis.vue";
+import MovieSynopsisTrailer from "@/components/MovieSynopsisTrailer.vue";
 export default {
   name: "MovieSynopsis",
   components: {
-    Movie_Synopsis
+    Movie_Synopsis,
+    MovieSynopsisTrailer
   }
 };
 </script>
