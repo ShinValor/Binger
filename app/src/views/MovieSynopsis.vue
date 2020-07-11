299df30bb92d9b0299df94745e78b55c56d849ca
LeKinchi <lesporis@gmail.com>
2020-07-10 23:32:03 -0400

    Made a new componenent

--- app/src/views/MovieSynopsis.vue
+++ app/src/views/MovieSynopsis.vue
@@ -2,18 +2,22 @@
   <a-layout :style="{ minHeight: '100%', overflow: 'auto' }">
     <h1>Movie Synopsis Page</h1>
     <Movie_Synopsis />
-    <MovieSynopsisTrailer />
+    <MovieTrailerCarousel />
+    <MovieSynopsisMovie />
+    <MovieSynopsisMovie listType="Similar Movies" />
   </a-layout>
 </template>
 
 <script scoped>
 import Movie_Synopsis from "@/components/MovieSynopsis.vue";
-import MovieSynopsisTrailer from "@/components/MovieSynopsisTrailer.vue";
+import MovieTrailerCarousel from "@/components/MovieTrailerCarousel.vue";
+import MovieSynopsisMovie from "@/components/MovieSynopsisMovie.vue";
 export default {
   name: "MovieSynopsis",
   components: {
     Movie_Synopsis,
-    MovieSynopsisTrailer
+    MovieTrailerCarousel,
+    MovieSynopsisMovie
   }
 };
 </script>
