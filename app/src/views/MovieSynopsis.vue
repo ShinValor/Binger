65e9daea2371ef6f5ca2422fd6ff26ac5c58bfe0
LeKinchi <lesporis@gmail.com>
2020-07-11 20:42:55 -0400

    Removed page header

--- app/src/views/MovieSynopsis.vue
+++ app/src/views/MovieSynopsis.vue
@@ -1,6 +1,5 @@
 <template>
   <a-layout :style="{ minHeight: '100%', overflow: 'auto' }">
-    <h1>Movie Synopsis Page</h1>
     <Movie_Synopsis />
     <MovieSynopsisMovieList />
     <MovieSynopsisMovieList listType="Similar Movies" />
