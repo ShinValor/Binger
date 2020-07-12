a457d09702b6f22bad370248f86ec27f0f155210
LeKinchi <32843930+LeKinchi@users.noreply.github.com>
2020-07-11 22:37:07 -0400

    Movie Synopsis Page Components (#85)
    * Created MovieSynopsis Component
    
    * Created MovieSynopsisTrailer component
    
    * Created carousel for movie trailers
    
    * Made some changes
    
    * Made a new componenent
    
    * Created MovieSynopsisCast componenet
    
    * Updated CSS
    
    * Made changes
    
    * Placed poster and title
    
    * added user interaction row
    
    * Added plot section
    
    * Finish header layout
    
    * Changed font color to white
    
    * Removed Movie Trailer carousell
    
    * Renamed MovieSynopsis component to MovieSynopsislist
    
    * Changed some CSS for the carousel
    
    * Rename component MovieSynopsisList to MovieSynopsisCastList
    
    * fixed error that occured b/c renaming
    
    * Updated layout of cast list componenet
    
    * Deleted old/unused componenets
    
    * Updated CSS for mobile responsiveness
    
    * Removed page header
    
    * Cleaned up CSS

---
+++ app/src/components/MovieSynopsisCastList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="card">
+    <h2 class="list-type-text">Cast List</h2>
+    <div>
+      <MovieSynopsisCast class="list-item" />
+      <MovieSynopsisCast class="list-item" />
+      <MovieSynopsisCast class="list-item" />
+      <MovieSynopsisCast class="list-item" />
+      <MovieSynopsisCast class="list-item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieSynopsisCast from "@/components/MovieSynopsisCast.vue";
+const listData = [];
+for (let i = 0; i < 10; i++) {
+  listData.push({
+    cast_id: 4,
+    character: "The Big Narrator",
+    credit_id: "52fe4250c3a36847f80149f3",
+    gender: 2,
+    id: 819,
+    name: "Edward Norton",
+    order: 0,
+    profile_path: "/eIkFHNlfretLS1spAcIoihKUS62.jpg"
+  });
+}
+
+export default {
+  name: "MovieSynopsisCastList",
+  components: {
+    MovieSynopsisCast
+  },
+  data() {
+    return {
+      listData,
+      pagination: {
+        onChange: page => {
+          console.log(page);
+        },
+        pageSize: 5
+      }
+    };
+  }
+};
+</script>
+<style scoped>
+.list-item {
+  display: inline-block;
+  margin: 10px;
+  padding: 2px;
+}
+.list-item:hover {
+  color: blue;
+  background-color: lightblue;
+}
+.card {
+  margin: 2em;
+}
+.list-type-text {
+  text-align: left;
+  font-size: 2em;
+  margin-bottom: 0%;
+  margin-top: 1rem;
+}
+</style>
--- app/src/views/MovieSynopsis.vue
+++ app/src/views/MovieSynopsis.vue
@@ -1,12 +1,23 @@
 <template>
   <a-layout :style="{ minHeight: '100%', overflow: 'auto' }">
-    <h1>This page will be built later</h1>
+    <Movie_Synopsis />
+    <MovieSynopsisMovieList />
+    <MovieSynopsisMovieList listType="Similar Movies" />
+    <MovieSynopsisCastList />
   </a-layout>
 </template>
 
 <script scoped>
+import Movie_Synopsis from "@/components/MovieSynopsis.vue";
+import MovieSynopsisMovieList from "@/components/MovieSynopsisMovieList.vue";
+import MovieSynopsisCastList from "@/components/MovieSynopsisCastList.vue";
 export default {
-  name: "MovieSynopsis"
+  name: "MovieSynopsis",
+  components: {
+    Movie_Synopsis,
+    MovieSynopsisMovieList,
+    MovieSynopsisCastList
+  }
 };
 </script>
 <style scoped></style>
