2d98325b83bd8ce2fcc984f43c74f9b1ce023301
LeKinchi <lesporis@gmail.com>
2020-07-25 02:50:28 -0400

    Created new page

---
+++ app/src/views/SearchResults.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-layout :style="{ minHeight: '100%', overflow: 'auto' }"></a-layout>
+</template>
+
+<script>
+export default {
+  name: "SearchResults",
+  components: {
+    SignupForm
+  }
+};
+</script>
+
+<style scoped></style>
