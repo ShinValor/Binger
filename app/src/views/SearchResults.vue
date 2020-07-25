b4d5e808a870cabfd84b38dd9b91df1700b5dd3a
LeKinchi <lesporis@gmail.com>
2020-07-25 03:10:09 -0400

    Updates

--- app/src/views/SearchResults.vue
+++ app/src/views/SearchResults.vue
@@ -1,12 +1,26 @@
 <template>
-  <a-layout :style="{ minHeight: '100%', overflow: 'auto' }"></a-layout>
+  <a-layout class="container">
+    <SearchBar :movieQuery="searchQuery" />
+    <Footer></Footer>
+  </a-layout>
 </template>
 
+
 <script>
+import SearchBar from "@/components/SearchBar.vue";
+
 export default {
   name: "SearchResults",
   components: {
-    SignupForm
+    SearchBar
+  },
+  computed: {
+    searchQuery: function() {
+      if ($route.query.movieQuery) {
+        return $route.query.movieQuery;
+      }
+      return "";
+    }
   }
 };
 </script>
